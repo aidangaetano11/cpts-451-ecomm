54d3a36584d634384efb2b14a254281967e93aea
aidangaetano11 <aidan.gaetano@wsu.edu>
2025-03-30 15:15:56 -0700

    Created customer service

---
+++ src/main/java/cpts451/ecomm/services/CustomerService.java
@@ -0,0 +1,11 @@
+﻿package cpts451.ecomm.services;
+
+import cpts451.ecomm.entities.Customer;
+
+public interface CustomerService {
+    Customer find(int id);
+
+    Iterable<Customer> findAll();
+
+    Customer save(Customer customer);
+}
