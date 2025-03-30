36d849c5b8ede0169896101012fe70f43c28a1f7
aidangaetano11 <aidan.gaetano@wsu.edu>
2025-03-30 15:55:30 -0700

    Created admin service

---
+++ src/main/java/cpts451/ecomm/services/AdminService.java
@@ -0,0 +1,11 @@
+﻿package cpts451.ecomm.services;
+
+import cpts451.ecomm.entities.Admin;
+
+public interface AdminService {
+    public Admin find(int id);
+
+    public Iterable<Admin> findAll();
+
+    public Admin save(Admin admin);
+}
