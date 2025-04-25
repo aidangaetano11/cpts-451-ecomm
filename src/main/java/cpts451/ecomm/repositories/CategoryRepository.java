98810cee32a4d888e01b3742b80a65efaf22d15d
aidangaetano11 <aidan.gaetano@wsu.edu>
2025-04-25 14:10:19 -0700

    Created category repository

---
+++ src/main/java/cpts451/ecomm/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+﻿package cpts451.ecomm.repositories;
+
+import cpts451.ecomm.entities.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer>  {
+}
