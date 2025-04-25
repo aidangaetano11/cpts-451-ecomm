feee75a4ac9fc2d9f126b40ac79911d60a02bff7
aidangaetano11 <aidan.gaetano@wsu.edu>
2025-04-25 14:01:58 -0700

    Added sorting of products on store page

--- src/main/java/cpts451/ecomm/controllers/StoreController.java
+++ src/main/java/cpts451/ecomm/controllers/StoreController.java
@@ -1,16 +1,26 @@
 package cpts451.ecomm.controllers;
 import cpts451.ecomm.entities.Admin;
 import cpts451.ecomm.entities.Customer;
+import cpts451.ecomm.entities.Product;
 import cpts451.ecomm.services.CartService;
 import cpts451.ecomm.services.ProductService;
 import cpts451.ecomm.services.ReviewProductService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 
 @Controller
+@RequestMapping("/storePage")
 public class StoreController {
     private final ProductService productService;
     private final CartService cartService;
@@ -22,12 +32,55 @@ public class StoreController {
         this.reviewProductService = reviewProductService;
     }
 
-    @RequestMapping("/storePage")
-    public String storePage(Model model) {
+    @GetMapping
+    public String showStorePage(@RequestParam(required = false) String sortBy, Model model, HttpSession session, Sort sort)
+    {
+        Iterable<Product> productIterable = productService.findAll();
+        List<Product> products = StreamSupport.stream(productIterable.spliterator(), false).collect(Collectors.toList()); // Converts iterable to list
 
-        model.addAttribute("products", productService.findAll());
+        if (sortBy != null)
+        {
+            products = sortProducts(products, sortBy);
+        }
+
+        model.addAttribute("products", products);
         model.addAttribute("reviewProduct", reviewProductService.findAll());
 
+
         return "storePage";
+    }
+
+    private List<Product> sortProducts(List<Product> products, String sortBy)
+    {
+        switch (sortBy) {
+            case "name":
+                return products.stream()
+                        .sorted(Comparator.comparing(Product::getProductName))
+                        .collect(Collectors.toList());
+            case "nameDesc":
+                return products.stream()
+                        .sorted(Comparator.comparing(Product::getProductName).reversed())
+                        .collect(Collectors.toList());
+            case "price":
+                return products.stream()
+                        .sorted(Comparator.comparing(Product::getProductPrice))
+                        .collect(Collectors.toList());
+            case "priceDesc":
+                return products.stream()
+                        .sorted(Comparator.comparing(Product::getProductPrice).reversed())
+                        .collect(Collectors.toList());
+            case "quantity":
+                return products.stream()
+                        .sorted(Comparator.comparing(Product::getProductQuantity))
+                        .collect(Collectors.toList());
+            case "quantityDesc":
+                return products.stream()
+                        .sorted(Comparator.comparing(Product::getProductQuantity).reversed())
+                        .collect(Collectors.toList());
+            default:
+                return products.stream()
+                        .sorted(Comparator.comparing(Product::getProductName))
+                        .collect(Collectors.toList());
+        }
     }
 }
