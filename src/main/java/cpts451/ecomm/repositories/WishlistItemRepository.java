866eb69f8bb1713c52171f9ce96758297bb1a50b
aidangaetano11 <aidan.gaetano@wsu.edu>
2025-04-26 13:19:53 -0700

    Added wishlist / wishlistItem repository and entities

---
+++ src/main/java/cpts451/ecomm/repositories/WishlistItemRepository.java
@@ -0,0 +1,6 @@
+﻿package cpts451.ecomm.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface WishlistItemRepository extends CrudRepository {
+}
---
+++ src/main/java/cpts451/ecomm/repositories/WishlistRepository.java
@@ -0,0 +1,11 @@
+﻿package cpts451.ecomm.repositories;
+
+import cpts451.ecomm.entities.Customer;
+import cpts451.ecomm.entities.Wishlist;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WishlistRepository extends CrudRepository {
+    @Query("SELECT w FROM Wishlist w WHERE w.customer = :customer")
+    Wishlist findByCustomer(Customer customer);
+}
