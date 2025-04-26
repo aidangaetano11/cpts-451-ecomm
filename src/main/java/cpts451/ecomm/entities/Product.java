667c3c1824c726bb1cbdb6d5042162d2f46e08cb
Albert Li <albertli2019@outlook.com>
2025-04-26 02:20:39 -0700

    Added variable to keep track of # of products sold, displayed on admin page

--- src/main/java/cpts451/ecomm/entities/Product.java
+++ src/main/java/cpts451/ecomm/entities/Product.java
@@ -28,10 +28,10 @@ public class Product {
     @Column(nullable = false)
     private Integer productQuantity;
 
-    /*
+
     @Column(nullable = false)
     private Integer productSold;
-    */
+
     @OneToMany(mappedBy = "product", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<ReviewProduct> reviewProduct = new ArrayList<>();
 
@@ -41,7 +41,16 @@ public class Product {
         this.productPrice = productPrice;
         this.productQuantity = productQuantity;
         this.productCategory = productCategory;
-        //this.productSold = 0;
+        this.productSold = 0;
+    }
+
+    public Product(String productName, String productDescription, Double productPrice, Integer productQuantity, Category productCategory, Integer productSold) {
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+        this.productCategory = productCategory;
+        this.productSold = productSold;
     }
 
     public Product() {
@@ -95,8 +104,8 @@ public class Product {
 
     public void setReviewProduct(List<ReviewProduct> reviewProduct) { this.reviewProduct = reviewProduct; }
 
-    /*
+
     public Integer getProductSold() { return productSold; }
 
-    public void setProductSold(Integer productSold) { this.productSold = productSold; }*/
+    public void setProductSold(Integer productSold) { this.productSold = productSold; }
 }
