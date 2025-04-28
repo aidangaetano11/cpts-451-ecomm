126ee83b2de22d70097503a259cc8fa414df67d7
Albert Li <albertli2019@outlook.com>
2025-04-27 17:17:36 -0700

    Deletion fixed by deleting OrderItems first (foreign key constraint)

--- src/main/java/cpts451/ecomm/repositories/OrderItemRepository.java
+++ src/main/java/cpts451/ecomm/repositories/OrderItemRepository.java
@@ -1,7 +1,14 @@
 package cpts451.ecomm.repositories;
 
 import cpts451.ecomm.entities.OrderItem;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderItemRepository extends CrudRepository<OrderItem, Integer> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM OrderItem i WHERE i.product.productID = :productID")
+    void deleteByProductId(int productID);
 }
